a38f1e6963dbfa04bc54687b83c8c3b860c130c7
Aryan Jaiswal <aryanjaiswal123123@gmail.com>
2025-10-05 00:03:59 +0530

    Add Swift playground with basic variable examples
    Introduced a new Swift playground demonstrating constants, variables, and basic data types. Also added necessary workspace and configuration files for the playground.

---
+++ SwiftCodePlayground.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+let pi:Double = 3.14 //Constant
+print(pi)
+
+var greeting = "Hello, playground"
+greeting = "Hello, I am aryan!"
+print(greeting)
+
+var myInt: Int = 6
+print(myInt)
+
+var myDouble: Double = 6.0
+print(myDouble)
+
+var myBool: Bool = true
+print(myBool)
+
