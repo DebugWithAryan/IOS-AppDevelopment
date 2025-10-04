414f423a5c736acdc8a818bad2d7e91dc91d20c7
Aryan Jaiswal <aryanjaiswal123123@gmail.com>
2025-10-04 23:37:01 +0530

    Enhance ContentView UI with ratings and details
    Updated ContentView to use a mint background, improved layout with alignment and spacing, added a ratings section with SF Symbols, review count, and additional icons for attractions. The view now features a styled card with shadow and rounded corners for better visual appeal.

--- My First App/My First App/ContentView.swift
+++ My First App/My First App/ContentView.swift
@@ -9,23 +9,62 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        
         ZStack{
-            Color(.black)
+            Color(.systemMint)
                 .ignoresSafeArea()
             
-            VStack{
+            VStack(alignment: .leading,spacing: 20.0){
                 Image("niagarafalls")
                     .resizable()
                     .cornerRadius(15)
                     .aspectRatio(contentMode: .fit)
-                    .padding(.all)
                 
-                Text("Niagara Falls")
-                    .font(Font.largeTitle)
-                    .fontWeight(Font.Weight.bold)
-                    .foregroundColor(.white)
+                HStack{
+                    Text("Niagara Falls")
+                        .font(Font.title)
+                        .fontWeight(Font.Weight.bold)
+                        .foregroundColor(.black)
+                    
+                    Spacer()
+                    
+                    VStack{
+                        //SF symbols
+                        HStack{
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.leadinghalf.filled")
+                            
+                        }
+                        .foregroundColor(.orange)
+                        .font(.caption)
+                        
+                        Text("(Reviews 361)")
+                            .font(.caption)
+                    }
+
+                }
+                
+                Text("Come visit the falls for an experience you won't forget!")
+                
+                HStack{
+                    Spacer()
+                    Image(systemName: "binoculars.fill")
+                    Image(systemName: "fork.knife")
+                }
+                .foregroundColor(.gray)
+                .font(Font.caption)
             }
+            .padding()
+            .background(Rectangle().foregroundColor(.white)
+                .cornerRadius(15)
+                .shadow(radius:15)
+            )
+            .padding()
         }
+        
     }
 }
 
