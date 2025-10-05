c80744b55d5d73138f82c33f280fea29898783db
Aryan Jaiswal <aryanjaiswal123123@gmail.com>
2025-10-05 21:23:55 +0530

    Add examples of parameterized and labeled functions in Swift
    Added new function examples demonstrating parameterized functions, functions with return values, and parameter labels in Swift. These examples illustrate different ways to define and call functions with parameters and return types.

--- SwiftCodePlayground.playground/Contents.swift
+++ SwiftCodePlayground.playground/Contents.swift
@@ -17,7 +17,6 @@ import UIKit
 
 
 //Function
-
 func hello(){
     var greeting = "Hello, I am aryan!"
     print(greeting)
@@ -25,8 +24,32 @@ func hello(){
 
 hello()
 
+//Parameterized Function
 func sayHello(m:String){
     print(m)
 }
 
 sayHello(m: "I am learning functions.")
+
+//Two Parameterized Function
+func doAdd(a:Int,b:Int){
+    var sum = a+b
+    print(sum)
+}
+doAdd(a: 4, b: 5)
+
+
+func doAddition(a:Int,b:Int) -> Int{
+    var sum = a+b
+    return sum
+}
+let result = doAddition(a: 7, b: 5)
+print(result)
+
+//Parameter Label
+func doAdditions(firstNum a:Int,secondNum b:Int) -> Int{
+    var sum = a+b
+    return sum
+}
+let results = doAdditions(firstNum: 7, secondNum: 10)
+print(results)
