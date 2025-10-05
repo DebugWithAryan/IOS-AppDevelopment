975281f981156bbc5ad1fc59379f70f77351b135
Aryan Jaiswal <aryanjaiswal123123@gmail.com>
2025-10-05 21:46:26 +0530

    Add state variables and deal button to ContentView
    Introduced playerCard, cpuCard, playerScore, and cpuScore variables to manage game state. Replaced static card images and scores with dynamic values, and added a deal button that triggers a new deal function.

--- War Card Game/War Card Game/ContentView.swift
+++ War Card Game/War Card Game/ContentView.swift
@@ -8,7 +8,16 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var playerCard : String = "card7"
+    var cpuCard : String = "card8"
+    
+    var playerScore: Int = 0
+    var cpuScore: Int = 0
+    
+    
     var body: some View {
+        
         ZStack{
             Image("background-plain")
                 .resizable()
@@ -20,13 +29,20 @@ struct ContentView: View {
                 Spacer()
                 HStack{
                     Spacer()
-                    Image("card2")
+                    Image(playerCard)
                     Spacer()
-                    Image("card3")
+                    Image(cpuCard)
                     Spacer()
                 }
                 Spacer()
-                Image ("button")
+                
+                Button {
+                    deal()
+                } label: {
+                    Image("button")
+                }
+
+                
                 Spacer()
                 HStack{
                     Spacer()
@@ -34,7 +50,7 @@ struct ContentView: View {
                         Text("Player")
                             .font(.headline)
                             .padding(.bottom,10.0)
-                        Text("0")
+                        Text(String(playerScore))
                             .font(.largeTitle)
                     }
                     Spacer()
@@ -42,7 +58,7 @@ struct ContentView: View {
                         Text("CPU")
                             .font(.headline)
                             .padding(.bottom,10.0)
-                        Text("0")
+                        Text(String(cpuScore))
                             .font(.largeTitle)
                     }
                     Spacer()
@@ -54,7 +70,13 @@ struct ContentView: View {
             }
         }
     }
+    
+    func deal(){
+        print("Deal Cards")
+    }
 }
+
+
 
 #Preview {
     ContentView()
